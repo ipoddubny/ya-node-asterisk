9741a82c9fa60f556eb52f8c490d00caa8983956
Ivan Poddubny <ivan.poddubny@gmail.com>
2020-12-04 21:48:46 +0100

    Add TypeScript type definitions

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare module 'yana' {
+  import { EventEmitter } from "events";
+
+  interface AMIOptions {
+    host?: string;
+    port?: number;
+    reconnect?: boolean;
+    events?: string | boolean;
+    login: string;
+  }
+
+  type ConnectCallback = (err: Error | undefined | null) => void;
+  type SendCallback = (err: Error | undefined | null, message: object) => void;
+  type DisconnectCallback = (err: Error | undefined | null) => void;
+
+  declare export default class AMI extends EventEmitter {
+    constructor(options: AMIOptions);
+    connect(cb?: ConnectCallback): Promise<void>;
+    send(action: object, cb?: SendCallback): Promise<object>;
+    disconnect(cb?: DisconnectCallback): Promise<void>;
+  }
+}
