0bf55a237610b3edf09357169b1fc1448807484a
Matt Busigin <mbusigin@citrisoft.com>
2021-01-04 22:18:32 -0500

    Added optional password to TS definition of AMIOptions.

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,7 @@ declare module 'yana' {
     reconnect?: boolean;
     events?: string | boolean;
     login: string;
+    password?: string;
   }
 
   type ConnectCallback = (err: Error | undefined | null) => void;
